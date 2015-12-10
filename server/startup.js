08393be7daee63357e838259e5cb5e30b53b92c2
markonovak <marko.novak@zemanta.com>
2015-12-10 14:33:33 +0100

    Initial commit

---
+++ server/startup.js
@@ -0,0 +1,15 @@
+if (Meteor.isServer) {
+    Meteor.startup(function () {
+        if (Images.find().count() == 0) {
+            for (var i=1; i<23; i++) {
+                Images.insert(
+                    {
+                        img_src: "img_" + i + ".jpg",
+                        img_alt: "image number " + i
+                    }
+                );
+            }
+            console.log(Images.find().count());
+        }
+    });
+}
